038f3054ba722cb655c392316f1f328e82a7a4e6
VanHell108 <80883697+VanHell108@users.noreply.github.com>
2022-10-07 06:03:13 +0300

    Commit All

---
+++ scripts/night.js
@@ -0,0 +1,6 @@
+function nightFunction() {
+  var element = document.body;
+  element.classList.toggle("night-mode");
+  element.classList.toggle("night-mode p");
+  element.classList.toggle("night-mode h2");
+}
---
+++ scripts/upBtn.js
@@ -0,0 +1,4 @@
+function upBtnFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
